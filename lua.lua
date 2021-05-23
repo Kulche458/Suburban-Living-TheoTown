098d6a182943b9f53d708eb37605cd2040dd67c8
Kulche458 <kulche458@gmail.com>
2021-05-23 10:18:08 +0200

    new variant, minor adjustments
    - a new house variant added
    - migrated settings to a separate script
    - fixed settings - works properly now

--- lua.lua
+++ lua.lua
@@ -1,31 +1,5 @@
 
-local settings
 math.randomseed(os.time())
-
-function script:init()
-    settings = Util.optStorage(TheoTown.getStorage(), self:getDraft():getId()..':settings')
-    settings.placeHedges = settings.placeHedges or 3
-    settings.alignToRoad = settings.alignToRoad or 2
-end
-
-function script:settings()
-    return {
-        {
-            name = 'Place hedges',
-            value = settings.placeHedges,
-            values = {1, 2, 3},
-            valueNames = {'Yes', 'No', 'Random'},
-            onChange = function(newState) settings.placeHedges = newState end
-        },
-        {
-            name = 'Align to roads',
-            value = settings.alignToRoad,
-            values = {1, 2},
-            valueNames = {'Yes', 'No'},
-            onChange = function(newState) settings.alignToRoad = newState end
-        }
-    }
-end
 
 local function colors(x, y)
     -- choose random wall color from 7 provided
--- settings.lua
+++ settings.lua
@@ -1,9 +1,8 @@
 
-local settings
-
 function script:init()
     settings = Util.optStorage(TheoTown.getStorage(), self:getDraft():getId()..':settings')
     settings.placeHedges = settings.placeHedges or 3
+    settings.alignToRoad = settings.alignToRoad or 2
 end
 
 function script:settings()
@@ -14,8 +13,13 @@ function script:settings()
             values = {1, 2, 3},
             valueNames = {'Yes', 'No', 'Random'},
             onChange = function(newState) settings.placeHedges = newState end
+        },
+        {
+            name = 'Align to roads',
+            value = settings.alignToRoad,
+            values = {1, 2},
+            valueNames = {'Yes', 'No'},
+            onChange = function(newState) settings.alignToRoad = newState end
         }
     }
 end
-
-
