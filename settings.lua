15da80eb95fec0fb2109fb0d6ab0fd6415e7d8fc
Kulche458 <kulche458@gmail.com>
2021-07-07 09:32:06 +0200

    disable/enable road setting
    script:enterCity because I ain't got time for this

--- settings.lua
+++ settings.lua
@@ -1,8 +1,12 @@
 
+local road, settings
+
 function script:init()
     settings = Util.optStorage(TheoTown.getStorage(), self:getDraft():getId()..':settings')
     settings.placeHedges = settings.placeHedges or 3
     settings.alignToRoad = settings.alignToRoad or 2
+    settings.showSuburbanRoad = settings.showSuburbanRoad or 2
+    road = Draft.getDraft('$kulche_suburbanroad00')
 end
 
 function script:settings()
@@ -20,6 +24,21 @@ function script:settings()
             values = {1, 2},
             valueNames = {'Yes', 'No'},
             onChange = function(newState) settings.alignToRoad = newState end
+        },
+        {
+            name = 'Show suburban road',
+            value = settings.showSuburbanRoad,
+            values = {1, 2},
+            valueNames = {'Yes', 'No'},
+            onChange = function(newState) settings.showSuburbanRoad = newState end
         }
     }
+end
+
+function script:enterCity()
+    if settings.showSuburbanRoad == 1 then
+        road:setVisible(true)
+    elseif settings.showSuburbanRoad == 2 then
+        road:setVisible(false)
+    end
 end
